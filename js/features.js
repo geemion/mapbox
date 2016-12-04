bf59d26d1bedd1c7dc4b23070b2faffd6b95d8d6
geemion <geemion@gmail.com>
2016-12-04 17:30:17 +0800

    revent

--- js/features.js
+++ js/features.js
@@ -1,173 +1,153 @@
 var geojson = [
 {
-  "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "properties": {
-          "id": "marker-isk0a6854",
-          "title": "通化 — 东北姑娘",
-          "details": "那一年我们都不知道对方的存在，若干年后对彼此许下一生的承诺",
-          "description": "<img src='https://oelwdg5d6.qnssl.com/b_home.jpg' alt='forbunny'>那一年我们都不知道对方的存在，若干年后对彼此许下一生的承诺",
-          "marker-size": "medium",
-          "marker-color": "#ffffff",
-          "marker-symbol": "warehouse"
-      },
-      "geometry": {
-          "coordinates": [
-          126.05,
-          42.68
-          ],
-          "type": "Point"
-      },
-      "id": "2f13a0089cef4d606e67ffa28df48c2a"
-  },
-  {
-      "type": "Feature",
-      "properties": {
-          "id": "marker-isk0lx6e6",
-          "title": "济宁 — 山东小伙",
-          "details": "那一年我们都不知道对方的存在，若干年后对彼此许下一生的承诺",
-          "description": "<img src='https://oelwdg5d6.qnssl.com/IMG_20150806_124627.jpg' alt='forbunny'>那一年我们都不知道对方的存在，若干年后对彼此许下一生的承诺",
-          "marker-size": "medium",
-          "marker-color": "#ffffff",
-          "marker-symbol": "warehouse"
-    },
-      "geometry": {
-          "coordinates": [
-          116.582794,
-          35.414795
-          ],
-          "type": "Point"
-      },
-      "id": "3607967b854de51956dbee94f5df64b7"
-  },
-  {
-      "type": "Feature",
-      "properties": {
-          "id": "marker-isk01a7p1",
-          "title": "青岛 — 相遇相爱篇",
-          "details": "风吹起你的头发，一张棕色的小网撒满我的面颊，我一生也不想挣脱",
-          "description": "<img src='https://oelwdg5d6.qnssl.com/74962bf9a53ceb34.jpg' alt='forbunny'>风吹起你的头发，一张棕色的小网撒满我的面颊，我一生也不想挣脱",
-          "marker-size": "medium",
-          "marker-color": "#ffffff",
-          "marker-symbol": "college"
-      },
-      "geometry": {
-          "coordinates": [
-          120.344238,
-          36.097938
-          ],
-          "type": "Point"
-      },
-      "id": "5595e5482935805cc25660574669bf86"
-  },
-  {
-      "type": "Feature",
-      "properties": {
-          "id": "marker-isjzvwlr0",
-          "title": "曲阜 — 游山玩水篇",
-          "details": "世界那么大，我们一起去看看，周游世界，然后记录下我们的足迹地图",
-          "description": "<img src='https://oelwdg5d6.qnssl.com/IMG20151003094748.jpg' alt='forbunny'>世界那么大，我们一起去看看，周游世界，然后记录下我们的足迹地图",
-          "marker-size": "medium",
-          "marker-color": "#ffffff",
-          "marker-symbol": "camera"
-      },
-      "geometry": {
-          "coordinates": [
-          116.9833,
-          35.6
-          ],
-          "type": "Point"
-      },
-      "id": "a13e67e2c59bc040d0fdc268474fb22b"
-  },
-  {
-      "type": "Feature",
-      "properties": {
-          "id": "marker-isk03qlp2",
-          "title": "北京 — 你在这",
-          "details": "1035公里的距离，短暂的相聚后留下的只是回忆，但是彼此的心连在一起",
-          "description": "<img src='https://oelwdg5d6.qnssl.com/IMG20150920065237.jpg' alt='forbunny'>1035公里的距离，短暂的相聚后留下的只是回忆，但是彼此的心连在一起",
-          "marker-size": "medium",
-          "marker-color": "#ffffff",
-          "marker-symbol": "rocket"
-      },
-      "geometry": {
-          "coordinates": [
-          116.38916,
-          39.918162
-          ],
-          "type": "Point"
-      },
-      "id": "b9c467e40ee87ba1c8dc0af537e0feec"
-  },
-  {
-      "type": "Feature",
-      "properties": {
-          "id": "marker-isk06d3h3",
-          "title": "西安 — 我在这",
-          "details": "1035公里的距离，短暂的相聚后留下的只是回忆，但是彼此的心连在一起",
-          "description": "<img src='https://oelwdg5d6.qnssl.com/IMG20160828044527.jpg' alt='forbunny'>1035公里的距离，短暂的相聚后留下的只是回忆，但是彼此的心连在一起",
-          "marker-size": "medium",
-          "marker-color": "#ffffff",
-          "marker-symbol": "rocket"
-      },
-      "geometry": {
-          "coordinates": [
-          108.929443,
-          34.375178
-          ],
-          "type": "Point"
-      },
-      "id": "4550dbd23b2eac654860ec82dfb2eb79"
-  },
-  {
-      "type": "Feature",
-      "properties": {
-          "id": "marker-isk0e8ce5",
-          "title": "西藏 — 格桑花",
-          "details": "你说你想去西藏看格桑花，朝圣，就像仓央嘉措的诗里说的：那一天，闭目在经殿的香雾中，蓦然听见你诵经的真言；那一月，我转动所有的转经筒，不为超度只为触摸你的指尖；那一年，我磕长头匍匐在山路，不为觐见只为贴着你的温暖；那一世，我转山转水转佛塔呀，不为修来世只为在途中与你相遇",
-          "description": "<img src='https://oelwdg5d6.qnssl.com/mp15776198_1432107541023_12.jpeg' alt='forbunny'>你说你想去西藏看格桑花，朝圣，就像仓央嘉措的诗里说的：那一天，闭目在经殿的香雾中，蓦然听见你诵经的真言；那一月，我转动所有的转经筒，不为超度只为触摸你的指尖；那一年，我磕长头匍匐在山路，不为觐见只为贴着你的温暖；那一世，我转山转水转佛塔呀，不为修来世只为在途中与你相遇",
-          "marker-size": "medium",
-          "marker-color": "#ffffff",
-          "marker-symbol": "golf"
-      },
-      "geometry": {
-          "coordinates": [
-          91.1,
-          29.6
-          ],
-          "type": "Point"
-      },
-      "id": "a859c2dc411102eb5907cbc4bd38e9af"
-  },
-  {
-      "type": "Feature",
-      "properties": {
-          "title": "你我之间的连线",
-          "details": "你我之间的连线",
-          "description": "1035公里的距离，短暂的相聚后留下的只是回忆，但是彼此的心连在一起",
-          "stroke": "#1087bf",
-          "stroke-width": 4,
-          "stroke-opacity": 1
-      },
-      "geometry": {
-          "coordinates": [
-          [
-          116.38916,
-          39.918162
-          ],
-          [
-          108.929443,
-          34.375178
-          ]
-          ],
-          "type": "LineString"
-      },
-      "id": "63150d05569345f5864e98bf255f7c6c"
-  }
-  ],
-  "id": "v4if.199afpm4"
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "marker-iwabqdez2",
+                "title": "泸州-你在的地方",
+                "details": "那一年我们都不知道对方的存在，若干年后对彼此许下一生的承诺",
+                "description": "<img src='http://7xkam4.com1.z0.glb.clouddn.com/xt.jpg' alt='forbunny'>那一年我们都不知道对方的存在，若干年后对彼此许下一生的承诺",
+                "marker-size": "medium",
+                "marker-color": "#ffffff",
+                "marker-symbol": "building"
+            },
+            "geometry": {
+                "coordinates": [
+                    105.588912,
+                    29.111375
+                ],
+                "type": "Point"
+            },
+            "id": "17b88bc0c58f87a6cc73bbe9b263d576"
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "marker-iwablvkd0",
+                "title": "绵阳-我成长的地方",
+                "details": "那一年我们都不知道对方的存在，若干年后对彼此许下一生的承诺",
+                "description": "<img src='http://7xkam4.com1.z0.glb.clouddn.com/my.jpg' alt='forbunny'>那一年我们都不知道对方的存在，若干年后对彼此许下一生的承诺",
+                "marker-size": "medium",
+                "marker-color": "#ffffff",
+                "marker-symbol": "building"
+            },
+            "geometry": {
+                "coordinates": [
+                    105.111007,
+                    30.825011
+                ],
+                "type": "Point"
+            },
+            "id": "41dc185d7c79c19bee6ee3c1e26cb254"
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "marker-iwabywy35",
+                "title": "成都-我在的地方",
+                "details": "400公里的距离，短暂的相聚后留下的只是回忆，但是彼此的心连在一起",
+                "description": "<img src='http://7xkam4.com1.z0.glb.clouddn.com/cd.jpg' alt='forbunny'>1035公里的距离，短暂的相聚后留下的只是回忆，但是彼此的心连在一起",
+                "marker-size": "medium",
+                "marker-color": "#ffffff",
+                "marker-symbol": "religious-jewish"
+            },
+            "geometry": {
+                "coordinates": [
+                    104.0625,
+                    30.675715
+                ],
+                "type": "Point"
+            },
+            "id": "249377146ce3323f8c553ef11fecb311"
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "marker-iwabs9mj3",
+                "title": "重庆-第一次出去玩的地方",
+                "details": "风吹起你的头发，一张棕色的小网撒满我的面颊，我一生也不想挣脱",
+                "description": "<img src='http://7xkam4.com1.z0.glb.clouddn.com/cq.jpg' alt='forbunny'>风吹起你的头发，一张棕色的小网撒满我的面颊，我一生也不想挣脱",
+                "marker-size": "medium",
+                "marker-color": "#ffffff",
+                "marker-symbol": "heart"
+            },
+            "geometry": {
+                "coordinates": [
+                    106.512451,
+                    29.547177
+                ],
+                "type": "Point"
+            },
+            "id": "36a0aefb191b6e0d83993fe861426c95"
+        },
+        
+        {
+            "type": "Feature",
+            "properties": {
+                "title": "",
+                "description": "",
+                "stroke": "#1087bf",
+                "stroke-width": 4,
+                "stroke-opacity": 0.9
+            },
+            "geometry": {
+                "coordinates": [
+                    [
+                        104.0625,
+                        30.666265
+                    ],
+                    [
+                        105.622558,
+                        29.084976
+                    ]
+                ],
+                "type": "LineString"
+            },
+            "id": "5f0533b28d7e7013a625a21beeab9a7a"
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "marker-iwabwtph4",
+                "title": "九寨沟-最美的地方留给最珍惜的人",
+                "details": "世界那么大，我们一起去看看，周游世界，然后记录下我们的足迹地图",
+                "description": "<img src='http://7xkam4.com1.z0.glb.clouddn.com/jz.jpg' alt='forbunny'>世界那么大，我们一起去看看，周游世界，然后记录下我们的足迹地图",
+                "marker-size": "medium",
+                "marker-color": "#ffffff",
+                "marker-symbol": "park"
+            },
+            "geometry": {
+                "coordinates": [
+                    103.903198,
+                    33.128351
+                ],
+                "type": "Point"
+            },
+            "id": "7abbc177b3ad834944f31c2cc5e31fbe"
+        }
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "marker-iwaettdx0",
+                "title": "宜宾-你成长的地方",
+                "details": "美丽的长江城市，成长了你",
+                "description": "<img src='http://7xkam4.com1.z0.glb.clouddn.com/yb.jpg' alt='forbunny'>美丽的长江城市，成长了你",
+                "marker-size": "medium",
+                "marker-color": "#ffffff",
+                "marker-symbol": "monument"
+            },
+            "geometry": {
+                "coordinates": [
+                    104.620056,
+                    28.760436
+                ],
+                "type": "Point"
+            },
+            "id": "3d48f69ce77934dbb9a1099e8eea1c55"
+        },
+    ],
+    "id": "maxyoung.29h1f9co"
 }
 ];
