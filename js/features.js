decdae86f43e3856bc31db91c69b9d244033e6ac
geemion <geemion@gmail.com>
2016-12-04 17:41:37 +0800

    js

--- js/features.js
+++ js/features.js
@@ -6,8 +6,9 @@ var geojson = [
             "type": "Feature",
             "properties": {
                 "id": "marker-iwabqdez2",
-                "title": "泸州妹子",
-                "description": "",
+                "title": "泸州-你在的地方",
+                "details": "那一年我们都不知道对方的存在，若干年后对彼此许下一生的承诺",
+                "description": "<img src='http://7xkam4.com1.z0.glb.clouddn.com/xt.jpg' alt='forbunny'>那一年我们都不知道对方的存在，若干年后对彼此许下一生的承诺",
                 "marker-size": "medium",
                 "marker-color": "#ffffff",
                 "marker-symbol": "building"
@@ -24,9 +25,30 @@ var geojson = [
         {
             "type": "Feature",
             "properties": {
+                "id": "marker-iwablvkd0",
+                "title": "绵阳-我成长的地方",
+                "details": "那一年我们都不知道对方的存在，若干年后对彼此许下一生的承诺",
+                "description": "<img src='http://7xkam4.com1.z0.glb.clouddn.com/my.jpg' alt='forbunny'>那一年我们都不知道对方的存在，若干年后对彼此许下一生的承诺",
+                "marker-size": "medium",
+                "marker-color": "#ffffff",
+                "marker-symbol": "building"
+            },
+            "geometry": {
+                "coordinates": [
+                    105.111007,
+                    30.825011
+                ],
+                "type": "Point"
+            },
+            "id": "41dc185d7c79c19bee6ee3c1e26cb254"
+        },
+        {
+            "type": "Feature",
+            "properties": {
                 "id": "marker-iwabywy35",
-                "title": "成都",
-                "description": "",
+                "title": "成都-我在的地方",
+                "details": "400公里的距离，短暂的相聚后留下的只是回忆，但是彼此的心连在一起",
+                "description": "<img src='http://7xkam4.com1.z0.glb.clouddn.com/cd.jpg' alt='forbunny'>1035公里的距离，短暂的相聚后留下的只是回忆，但是彼此的心连在一起",
                 "marker-size": "medium",
                 "marker-color": "#ffffff",
                 "marker-symbol": "religious-jewish"
@@ -44,8 +66,9 @@ var geojson = [
             "type": "Feature",
             "properties": {
                 "id": "marker-iwabs9mj3",
-                "title": "重庆",
-                "description": "",
+                "title": "重庆-第一次出去玩的地方",
+                "details": "风吹起你的头发，一张棕色的小网撒满我的面颊，我一生也不想挣脱",
+                "description": "<img src='http://7xkam4.com1.z0.glb.clouddn.com/cq.jpg' alt='forbunny'>风吹起你的头发，一张棕色的小网撒满我的面颊，我一生也不想挣脱",
                 "marker-size": "medium",
                 "marker-color": "#ffffff",
                 "marker-symbol": "heart"
@@ -59,44 +82,7 @@ var geojson = [
             },
             "id": "36a0aefb191b6e0d83993fe861426c95"
         },
-        {
-            "type": "Feature",
-            "properties": {
-                "id": "marker-iwaettdx0",
-                "title": "宜宾",
-                "description": "",
-                "marker-size": "medium",
-                "marker-color": "#ffffff",
-                "marker-symbol": "monument"
-            },
-            "geometry": {
-                "coordinates": [
-                    104.620056,
-                    28.760436
-                ],
-                "type": "Point"
-            },
-            "id": "3d48f69ce77934dbb9a1099e8eea1c55"
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "id": "marker-iwablvkd0",
-                "title": "绵阳小伙",
-                "description": "",
-                "marker-size": "medium",
-                "marker-color": "#ffffff",
-                "marker-symbol": "building"
-            },
-            "geometry": {
-                "coordinates": [
-                    105.111007,
-                    30.825011
-                ],
-                "type": "Point"
-            },
-            "id": "41dc185d7c79c19bee6ee3c1e26cb254"
-        },
+        
         {
             "type": "Feature",
             "properties": {
@@ -125,8 +111,9 @@ var geojson = [
             "type": "Feature",
             "properties": {
                 "id": "marker-iwabwtph4",
-                "title": "九寨沟",
-                "description": "",
+                "title": "九寨沟-最美的地方留给最珍惜的人",
+                "details": "世界那么大，我们一起去看看，周游世界，然后记录下我们的足迹地图",
+                "description": "<img src='http://7xkam4.com1.z0.glb.clouddn.com/jz.jpg' alt='forbunny'>世界那么大，我们一起去看看，周游世界，然后记录下我们的足迹地图",
                 "marker-size": "medium",
                 "marker-color": "#ffffff",
                 "marker-symbol": "park"
@@ -140,6 +127,26 @@ var geojson = [
             },
             "id": "7abbc177b3ad834944f31c2cc5e31fbe"
         }
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "marker-iwaettdx0",
+                "title": "宜宾-你成长的地方",
+                "details": "美丽的长江城市，成长了你",
+                "description": "<img src='http://7xkam4.com1.z0.glb.clouddn.com/yb.jpg' alt='forbunny'>美丽的长江城市，成长了你",
+                "marker-size": "medium",
+                "marker-color": "#ffffff",
+                "marker-symbol": "monument"
+            },
+            "geometry": {
+                "coordinates": [
+                    104.620056,
+                    28.760436
+                ],
+                "type": "Point"
+            },
+            "id": "3d48f69ce77934dbb9a1099e8eea1c55"
+        },
     ],
     "id": "maxyoung.29h1f9co"
 }
