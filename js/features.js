7b486cc4d53b5ff935fead49fd549516ecf2cce7
geemion <geemion@gmail.com>
2019-02-16 19:50:38 +0800

    add sz/xa

--- js/features.js
+++ js/features.js
@@ -8,7 +8,7 @@ var geojson = [
                 "id": "marker-iwabqdez2",
                 "title": "泸州-你在这",
                 "details": "那一年我们都不知道对方的存在，若干年后对彼此许下一生的承诺",
-                "description": "<img src='http://7xkam4.com1.z0.glb.clouddn.com/xt.jpg' alt='forbunny'>那一年我们都不知道对方的存在，若干年后对彼此许下一生的承诺",
+                "description": "<img src='/img/xt.jpg' alt='forbunny'>那一年我们都不知道对方的存在，若干年后对彼此许下一生的承诺",
                 "marker-size": "medium",
                 "marker-color": "#f86767",
                 "marker-symbol": "building"
@@ -28,7 +28,7 @@ var geojson = [
                 "id": "marker-iwablvkd0",
                 "title": "绵阳",
                 "details": "那一年我们都不知道对方的存在，若干年后对彼此许下一生的承诺",
-                "description": "<img src='http://7xkam4.com1.z0.glb.clouddn.com/my.jpg' alt='forbunny'>那一年我们都不知道对方的存在，若干年后对彼此许下一生的承诺",
+                "description": "<img src='/img/my.jpg' alt='forbunny'>那一年我们都不知道对方的存在，若干年后对彼此许下一生的承诺",
                 "marker-size": "medium",
                 "marker-color": "#f86767",
                 "marker-symbol": "building"
@@ -48,7 +48,7 @@ var geojson = [
                 "id": "marker-iwabywy35",
                 "title": "成都-我在这",
                 "details": "300公里的距离，短暂的相聚后留下的只是回忆，但是彼此的心连在一起",
-                "description": "<img src='http://7xkam4.com1.z0.glb.clouddn.com/cd.jpg' alt='forbunny'>300公里的距离，短暂的相聚后留下的只是回忆，但是彼此的心连在一起",
+                "description": "<img src='/img/cd.jpg' alt='forbunny'>300公里的距离，短暂的相聚后留下的只是回忆，但是彼此的心连在一起",
                 "marker-size": "medium",
                 "marker-color": "#f86767",
                 "marker-symbol": "religious-jewish"
@@ -68,7 +68,7 @@ var geojson = [
                 "id": "marker-iwabs9mj3",
                 "title": "重庆-犹忆当年一相逢, 万世此心与君同",
                 "details": "风吹起你的头发，一张棕色的小网撒满我的面颊，我一生也不想挣脱",
-                "description": "<img src='http://7xkam4.com1.z0.glb.clouddn.com/cq.jpg' alt='forbunny'>风吹起你的头发，一张棕色的小网撒满我的面颊，我一生也不想挣脱",
+                "description": "<img src='/img/cq.jpg' alt='forbunny'>风吹起你的头发，一张棕色的小网撒满我的面颊，我一生也不想挣脱",
                 "marker-size": "medium",
                 "marker-color": "#f86767",
                 "marker-symbol": "heart"
@@ -88,7 +88,7 @@ var geojson = [
                 "id": "marker-iwabwtph4",
                 "title": "九寨沟-最美的地方留给最珍惜的人",
                 "details": "世界那么大，我们一起去看看，周游世界，然后记录下我们的足迹地图",
-                "description": "<img src='http://7xkam4.com1.z0.glb.clouddn.com/jz.jpg' alt='forbunny'>世界那么大，我们一起去看看，周游世界，然后记录下我们的足迹地图",
+                "description": "<img src='/img/jz.jpg' alt='forbunny'>世界那么大，我们一起去看看，周游世界，然后记录下我们的足迹地图",
                 "marker-size": "medium",
                 "marker-color": "#f86767",
                 "marker-symbol": "park"
@@ -108,7 +108,7 @@ var geojson = [
                 "id": "marker-iwaettdx0",
                 "title": "宜宾",
                 "details": "长江边美丽的小城，成长了你",
-                "description": "<img src='http://7xkam4.com1.z0.glb.clouddn.com/yb.jpg' alt='forbunny'>长江边美丽的小城，成长了你",
+                "description": "<img src='/img/yb.jpg' alt='forbunny'>长江边美丽的小城，成长了你",
                 "marker-size": "medium",
                 "marker-color": "#f86767",
                 "marker-symbol": "monument"
@@ -127,8 +127,8 @@ var geojson = [
             "properties": {
                 "id": "marker-iwaettdx0",
                 "title": "香港",
-                "details": "香港美好的回忆，感受资本主义",
-                "description": "<img src='http://7xkam4.com1.z0.glb.clouddn.com/yb.jpg' alt='forbunny'>长江边美丽的小城，成长了你",
+                "details": "2017.9 短暂相聚",
+                "description": "<img src='/img/hk.jpg' alt='forbunny'>香港感受资本主义",
                 "marker-size": "medium",
                 "marker-color": "#f86767",
                 "marker-symbol": "monument"
@@ -137,6 +137,66 @@ var geojson = [
                 "coordinates": [
                     114.10000,
                     22.20000
+                ],
+                "type": "Point"
+            },
+            "id": "3d48f69ce77934dbb9a1099e8eea1c55"
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "marker-iwaettdx0",
+                "title": "深圳",
+                "details": "工作半年，无限思念",
+                "description": "<img src='/img/sz.jpg' alt='forbunny'>鹅厂，再见！",
+                "marker-size": "medium",
+                "marker-color": "#f86767",
+                "marker-symbol": "monument"
+            },
+            "geometry": {
+                "coordinates": [
+                    113.554680,
+                    22.323643
+                ],
+                "type": "Point"
+            },
+            "id": "3d48f69ce77934dbb9a1099e8eea1c55"
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "marker-iwaettdx0",
+                "title": "西安",
+                "details": "周末游六朝古都",
+                "description": "<img src='/img/xa.jpg' alt='forbunny'>西安、华山",
+                "marker-size": "medium",
+                "marker-color": "#f86767",
+                "marker-symbol": "monument"
+            },
+            "geometry": {
+                "coordinates": [
+                    108.554680,
+                    33.760436
+                ],
+                "type": "Point"
+            },
+            "id": "3d48f69ce77934dbb9a1099e8eea1c55"
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "marker-iwaettdx0",
+                "title": "达古冰川",
+                "details": "团建一起出游",
+                "description": "<img src='/img/dgbc.jpg' alt='forbunny'>无限风景",
+                "marker-size": "medium",
+                "marker-color": "#f86767",
+                "marker-symbol": "monument"
+            },
+            "geometry": {
+                "coordinates": [
+                    102.484680,
+                    32.153036
                 ],
                 "type": "Point"
             },
